11ff123c063a25844ea942bd22d7f66677226200
Miro Kropacek <miro.kropacek@gmail.com>
2019-04-02 21:30:27 +0200

    save/restore cache

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 void save_videl(void);
 void restore_videl(void);
+
+void save_cache(void);
 void flush_cache(void);
+void restore_cache(void);
+
 void picture_boot(void);
 
 static long old_ssp;
@@ -11,10 +15,12 @@ int main(int argc, const char* argv[])
 {
 	old_ssp = Super(0L);
 	save_videl();
+	save_cache();
 	flush_cache();
 
 	picture_boot();
 
+	restore_cache();
 	restore_videl();
 	Super(old_ssp);
 
